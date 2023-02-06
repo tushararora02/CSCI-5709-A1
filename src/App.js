0ad8ccdb217ffdbe823fb4c809cf867f96bee84d
tarora <tarora>
2023-02-06 17:47:31 -0400

    changes in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import './App.css';
-import { Route, Routes} from 'react-router-dom';
+import { BrowserRouter , Route, Routes} from 'react-router-dom';
 import Login from './login';
 import Registration from './Registration';
 import Home from './Home';
 import Reset from './reset';
 function App() {
   return (
-    
+    <BrowserRouter>
  <Routes>
+  <Route index element={<Home/>}/> 
   <Route path='/' element={<Home/>}/> 
+
   <Route path='/login' element={<Login/>}/> 
   <Route path='/register' element={<Registration/>}/>
   <Route path='/reset' element={<Reset/>}/>
  </Routes>
+ </BrowserRouter>
+
  
   );
 }
