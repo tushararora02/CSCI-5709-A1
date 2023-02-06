c06e8a3ba2bfead76caf0da502edbe537ec2e62a
tarora <tarora>
2023-02-06 16:53:39 -0400

    updated files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { Route, Routes} from 'react-router-dom';
 import Login from './login';
 import Registration from './Registration';
 import Home from './Home';
 import Reset from './reset';
 function App() {
   return (
+    
  <Routes>
   <Route path='/' element={<Home/>}/> 
   <Route path='/login' element={<Login/>}/> 
   <Route path='/register' element={<Registration/>}/>
   <Route path='/reset' element={<Reset/>}/>
  </Routes>
+ 
   );
 }
 
--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,93 @@
 import React from 'react';
 import logo from "./image/logo.png";
 import './App.css';
-
-class Home extends React.Component{
-
-    constructor() {
-        super();
-    }
+import {useNavigate} from "react-router-dom";
 
 
-    render(){
+function Home() {
+    const navigate = useNavigate();
+    
+    const goTologin = () => {
+    
+      // This will navigate to second component
+      navigate('/login'); 
+    };
+    const goToRegistration = () => {
+    
+      // This will navigate to first component
+      navigate('/register'); 
+    };
+    
+    return (
+        <div id="home">
+        <h3 style={{fontSize:'24px', textAlign:'center'}}>Logo</h3>
+        <img src={logo} width="175" height="175" style={{marginLeft:"60px"}}/>
+        <div className='buttons'>
+         <button onClick={goTologin}  className='home-button'>Login</button>
+         <button onClick={goToRegistration} className='home-button'>Register</button>
+        
+         </div>
+         
+     </div>
+    );
+  }
+    
+  export default Home;
 
-        return (
-            <div id="home">
-               <h3 style={{fontSize:'24px', textAlign:'center'}}>Logo</h3>
-               <img src={logo} width="175" height="175" style={{marginLeft:"60px"}}/>
-               <div className='buttons'>
-                <a href='/login' className='home-button'>Login</a>
-                <a href='register' className='home-button'>Register</a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function  change() {
+//     const navigate = useNavigate();
+    
+//     const goToSecondsComp = () => {
+    
+//       // This will navigate to second component
+//       navigate('/login'); 
+//     };
+//     const gotToFirstComp = () => {
+    
+//       // This will navigate to first component
+//       navigate('/register'); 
+//     };
+// }
+
+
+// class Home extends React.Component{
+
+    
+//     constructor() {
+//         super();
+      
+//     }
+    
+
+//     render(){
+
+//         return (
+//             <div id="home">
+//                <h3 style={{fontSize:'24px', textAlign:'center'}}>Logo</h3>
+//                <img src={logo} width="175" height="175" style={{marginLeft:"60px"}}/>
+//                <div className='buttons'>
+//                 <button onClick={gotToFirstComp}  className='home-button'>Login</button>
+//                 <a href='register' className='home-button'>Register</a>
                
-                </div>
+//                 </div>
                 
-            </div>
+//             </div>
 
-        );
-    }
+//         );
+//     }
 
-}
-export default Home;
+// }
+// export default Home;
